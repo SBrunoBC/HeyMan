2479914c9bd195725914e423da8ed4f963469616
Sal Bruno <boozecode.reload@gmail.com>
2017-09-18 04:43:38 -0700

    Create index.php
    Added html skeleton.

---
+++ html/index.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Author: Sal Bruno
+ * Date: September - 2017
+ * Copyright: Copyright 2017, Sal Bruno
+ * License: MIT (For full version see LICENSE.txt)
+ *
+ * Main index file for HeyMan CMS.
+ */
+ ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="Sal Bruno">
+
+    <title>Heyman CMS</title>
+
+    <link rel="stylesheet" href="assets/styles/main.css">
+    <link rel="stylesheet" href="assets/styles/grid.css">
+</head>
+<body>
+</body>
+</html>
