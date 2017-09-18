8141c860da648db844962a67b0e8f75b942ecb8c
Sal Bruno <boozecode.reload@gmail.com>
2017-09-18 05:08:39 -0700

    Update connect_db.php

--- html/assets/include/connect_db.php
+++ html/assets/include/connect_db.php
@@ -8,6 +8,16 @@
  * Connects to database with details in /config/hm_config.ini
  */
 
+if ($isIndexToDB != TRUE) {
+    exit("<h2>You cannot access this file directly!</h2>");
+}
+
+// Sets database details
+$dbConfig = parse_ini_file("../config/hm_config.ini");
+
+// Connect to database
+$connection = mysqli_connect("localhost", $dbConfig["user"], $dbConfig["password"], $dbConfig["dbname"]);
+
 // Sets database details
 $dbConfig = parse_ini_file("../config/hm_config.ini");
 
